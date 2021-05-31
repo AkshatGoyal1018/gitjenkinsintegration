b8bd5900e265a190995f1f3ae66e80349a21f64b
AkshatGoyal <58101087+AkshatGoyal1018@users.noreply.github.com>
2021-05-31 14:43:56 +0530

    Create demo.java

---
+++ demo.java
@@ -0,0 +1,13 @@
+public class MyProg1
+{
+	public static void main(String arg[])
+	{
+		/*int i=Integer.parseInt(arg[0]);
+		int j=Integer.parseInt(arg[1]);*/
+		int i=12;
+		for(int k=1;k<=10;k++)
+		{
+			System.out.println(i+"*"+k+"="+(i*k));
+		}
+	}
+}
